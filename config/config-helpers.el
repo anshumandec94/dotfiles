c06e0de6f204bb38e0796398e2566fbb64b0f5e6
Anshuman Narayan <anshu.nryn@gmail.com>
2026-05-23 14:34:29 -0400

    fix(issue-1): one-step notebook creation in monorepo sub-projects

--- config/config-helpers.el
+++ config/config-helpers.el
@@ -8,25 +8,63 @@
 
 
 
+(defcustom my/notebook-monorepo-root
+  "/Users/anara0823/Projects/data-strategy-models/"
+  "Root directory of the monorepo containing notebook sub-projects."
+  :type 'directory
+  :group 'convenience)
+
+(defcustom my/notebook-monorepo-projects-subdir
+  "projects/"
+  "Subdirectory under the monorepo root that contains sub-projects."
+  :type 'string
+  :group 'convenience)
+
+(defun my/--current-subproject-root ()
+  "Return the current sub-project root inside the monorepo, or nil.
+Inferred from `default-directory' as
+`<monorepo-root>/<projects-subdir>/<sub-project>/'."
+  (let* ((root (file-name-as-directory
+                (expand-file-name my/notebook-monorepo-root)))
+         (projects-dir (file-name-as-directory
+                        (expand-file-name my/notebook-monorepo-projects-subdir
+                                          root)))
+         (cwd (file-name-as-directory (expand-file-name default-directory))))
+    (when (string-prefix-p projects-dir cwd)
+      (let* ((rel (substring cwd (length projects-dir)))
+             (first-component (car (split-string rel "/" t))))
+        (when first-component
+          (file-name-as-directory
+           (expand-file-name first-component projects-dir)))))))
+
 (defun my/create-tmp-notebook (name)
-  "Create a new Python notebook in the current project's notebooks directory."
+  "Create a new Python notebook in the current sub-project's notebooks/ dir.
+Sub-project is inferred from the current buffer's path inside
+`<monorepo-root>/<projects-subdir>/<sub-project>/'. Auto-creates a
+.projectile marker at the sub-project root and the notebooks/ subdir
+if either is missing."
   (interactive "sNotebook name: ")
-  (let* ((project-root (or (and (fboundp 'projectile-project-root)
-                                (projectile-project-root))
-                           default-directory))
+  (let* ((subroot (or (my/--current-subproject-root)
+                      (user-error
+                       "Not inside a sub-project under %s%s"
+                       my/notebook-monorepo-root
+                       my/notebook-monorepo-projects-subdir)))
+         (projectile-marker (expand-file-name ".projectile" subroot))
+         (notebooks-dir (file-name-as-directory
+                         (expand-file-name "notebooks" subroot)))
          (date-prefix (format-time-string "%Y%m%d"))
-         (notebooks-dir (concat project-root "notebooks/"))
-         (filename (format "%s%s_%s.org" notebooks-dir date-prefix name)))
-    ;; Create notebooks directory if it doesn't exist
+         (filename (expand-file-name (format "%s_%s.org" date-prefix name)
+                                     notebooks-dir)))
+    (unless (file-exists-p projectile-marker)
+      (write-region "" nil projectile-marker)
+      (message "Created .projectile at %s" subroot))
     (unless (file-exists-p notebooks-dir)
       (make-directory notebooks-dir t))
-    ;; Create and open the file
     (find-file filename)
-    ;; Only insert snippet if file is new/empty
     (when (= (buffer-size) 0)
       (org-mode)
-      (unless yas-minor-mode w
-              (yas-global-mode  1))
+      (unless yas-minor-mode
+        (yas-global-mode 1))
       (insert "babel")
       (yas-expand))))
 
